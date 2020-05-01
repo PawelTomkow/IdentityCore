3ae832e07de5866ffddb2e2d7bf9a2ec22115600
Pawel Tomkow <pawel.tomkow@teleste.com>
2020-05-01 22:47:12 +0200

    [Refactor]Clean up Startup.cs

--- src/Identity/Startup.cs
+++ src/Identity/Startup.cs
@@ -39,13 +39,53 @@ namespace Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            InitializeConfigurations(services);
-            RegisterConfigurations(services);
-            RegisterSwagger(services);
-            RegisterContext(services);
-            RegisterServices(services);
-            ConfigureControllers(services);
-            ConfigureCors(services);
+            
+            services.AddControllers(options =>
+            {
+                options.RespectBrowserAcceptHeader = false;
+                options.ReturnHttpNotAcceptable = false;
+            });
+
+            services.AddSingleton(Configuration);
+
+            services
+                .Configure<CacheSettings>(
+                    Configuration.GetSection(nameof(CacheSettings)))
+                .Configure<SecuritySettings>(
+                    Configuration.GetSection(nameof(SecuritySettings)))
+                .Configure<DatabaseConfig>(
+                    Configuration.GetSection(nameof(DatabaseConfig)));
+            
+            services
+                .AddSingleton(sp =>
+                    sp.GetRequiredService<IOptions<CacheSettings>>().Value)
+                .AddSingleton(sp =>
+                    sp.GetRequiredService<IOptions<SecuritySettings>>().Value)
+                .AddSingleton(sp =>
+                    sp.GetRequiredService<IOptions<DatabaseConfig>>().Value);
+            
+            services
+                .AddMemoryCache()
+                .AddCustomContext(Configuration, _projectName)
+                .AddSingleton<ICache, MemoryCache>()
+                .AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>()
+                .AddAutoMapper()
+                .AddIdentityRepositories()
+                .AddIdentityServices()
+                .AddSwaggerGen(
+                    c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "HomeBackend", Version = "v1"}); })
+                .AddCors(options =>
+                {
+                    options.AddPolicy("AllowAll",
+                        builder =>
+                        {
+                            builder
+                                .AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader();
+                            // .AllowCredentials(); //TODO: turn on when add auth!
+                        });
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -69,89 +109,5 @@ namespace Identity
             app.UseCors("AllowAll")
                 .UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
-
-        private static void ConfigureControllers(IServiceCollection services)
-        {
-            services.AddControllers(options =>
-            {
-                options.RespectBrowserAcceptHeader = false;
-                options.ReturnHttpNotAcceptable = false;
-            });
-        }
-
-        private static void ConfigureCors(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    builder =>
-                    {
-                        builder
-                            .AllowAnyOrigin()
-                            .AllowAnyMethod()
-                            .AllowAnyHeader();
-                        // .AllowCredentials(); //TODO: turn on when add auth!
-                    });
-            });
-        }
-
-        private void RegisterContext(IServiceCollection services)
-        {
-            services.AddDbContext<IdentityContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("IdentityDatabase"), x
-                    => x.MigrationsAssembly(_projectName)));
-        }
-
-        private static void RegisterSwagger(IServiceCollection services)
-        {
-            services.AddSwaggerGen(
-                c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "HomeBackend", Version = "v1"}); });
-        }
-
-        private static void RegisterConfigurations(IServiceCollection services)
-        {
-            services.AddSingleton(sp =>
-                sp.GetRequiredService<IOptions<CacheSettings>>().Value);
-            services.AddSingleton(sp =>
-                sp.GetRequiredService<IOptions<SecuritySettings>>().Value);
-            services.AddSingleton(sp =>
-                sp.GetRequiredService<IOptions<DatabaseConfig>>().Value);
-        }
-
-        private void InitializeConfigurations(IServiceCollection services)
-        {
-            services.AddSingleton(Configuration);
-
-            services.Configure<CacheSettings>(
-                Configuration.GetSection(nameof(CacheSettings)));
-            services.Configure<SecuritySettings>(
-                Configuration.GetSection(nameof(SecuritySettings)));
-            services.Configure<DatabaseConfig>(
-                Configuration.GetSection(nameof(DatabaseConfig)));
-        }
-
-        private void RegisterServices(IServiceCollection services)
-        {
-            services.AddMemoryCache();
-            
-            RegisterIdentity(services);
-            RegisterPersistence(services);
-        }
-
-        private void RegisterPersistence(IServiceCollection services)
-        {
-            services.AddSingleton<ICache, MemoryCache>();
-            services
-                .AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
-        }
-
-        private void RegisterIdentity(IServiceCollection services)
-        {
-            services
-                .AddAutoMapper()
-                .AddIdentityRepositories()
-                .AddIdentityServices();
-        }
-        
     }
 }
