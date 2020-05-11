fdd0228b1a843acc3dd48ca70836223794812cb0
Pawel Tomkow <pawel.tomkow@teleste.com>
2020-05-11 21:25:27 +0200

    [token]remove unused data from token model.

--- src/Identity.Application/DTOs/TokenDto.cs
+++ src/Identity.Application/DTOs/TokenDto.cs
@@ -6,15 +6,7 @@ namespace Identity.Application.DTOs
 {
     public class TokenDto : ICommand
     {
-        [JsonProperty("tokenId")] public int TokenId { get; set; }
-        [JsonProperty("idSession")] public string IdSession { get; set; }
-
         [JsonProperty("accessToken")] public string AccessToken { get; set; }
-
         [JsonProperty("refreshToken")] public string RefreshToken { get; set; }
-
-        [JsonProperty("experienceTime")] public long ExperienceTime { get; set; }
-
-        [JsonProperty("claims")] public List<string> Claims { get; set; }
     }
 }
